16f8a75370ba5973180bd20d6c29da38e79a0f80
William Denorme <80251305+Williamesta@users.noreply.github.com>
2021-05-28 11:41:51 +0200

    nombre bonus

--- options jeu.c
+++ options jeu.c
@@ -2,3 +2,5 @@
 #include <stdlib.h>
 #define MAX 9
 typedef int Grid[MAX][MAX]
+
+//Pour la fonction qui place un nombre aléatoire sur la grille, on utilise la fonction randomGenerate avec un bnClues=1
