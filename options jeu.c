56864627bdcff0b0d9fdbc829e3b771e760f4296
William Denorme <80251305+Williamesta@users.noreply.github.com>
2021-05-28 11:55:43 +0200

    détecteur d'erreur

--- options jeu.c
+++ options jeu.c
@@ -4,3 +4,5 @@
 typedef int Grid[MAX][MAX]
 
 //Pour la fonction qui place un nombre aléatoire sur la grille, on utilise la fonction randomGenerate avec un bnClues=1
+
+//Pour la fonction de détection d'erreur à chaque tout, on utilise la fonction isValidMove
