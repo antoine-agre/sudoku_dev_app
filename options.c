8b5b04937b87a444235e47d0b73a74edbc67f604
antoine-agre <80821251+antoine-agre@users.noreply.github.com>
2021-06-02 09:23:12 +0200

    Rajout ";" manquant

--- options.c
+++ options.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #define MAX 9
 
-typedef int Grid[MAX][MAX]
+typedef int Grid[MAX][MAX];
 
 //Pour la fonction qui place un nombre aléatoire sur la grille, on utilise la fonction randomGenerate avec un bnClues=1
 
