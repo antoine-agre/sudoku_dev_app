6825d3cf6f49a3800ce5b65ace1fe2e47da3b430
khabizard <42652673+khabizard@users.noreply.github.com>
2021-06-02 12:20:02 +0200

    Changement de l'affichage dans le fichier texte

--- fichiertxt.c
+++ fichiertxt.c
@@ -16,19 +16,19 @@ void saveGrid(Grid grille){
     strcat(name, ".txt");
     fichier = fopen(name, "w");
     //printf("\n\n");
-    fputs("\n\n", fichier);
+    fputs("\n", fichier);
     for (int i = 0; i < MAX; i++){
         for(int j = 0; j < MAX; j++){
             if(grille[i][j] == 0){
                     //printf(".\t");
-                    fputs(".\t", fichier);}
+                    fputs("0", fichier);}
             else {
                     //printf("%d\t", grille[i][j]);
-                    fprintf(fichier,"%d\t", grille[i][j]);
+                    fprintf(fichier,"%d", grille[i][j]);
             }
         }
         //printf("\n\n");
-        fputs("\n\n", fichier);
+        fputs("\n", fichier);
     }
 
 }
