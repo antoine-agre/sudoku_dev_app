7355bbad2ac798e49ea75692311fc07d61eb7068
William Denorme <80251305+Williamesta@users.noreply.github.com>
2021-06-09 10:55:38 +0200

    les accents cest pas ouf

--- Possibilités de jeu.c
+++ Possibilités de jeu.c
@@ -7,13 +7,13 @@ typedef int Grid[MAX][MAX];
 
 void rules(){
     printf("\nLe Sudoku\n");
-    printf("Les règles sont les suivantes :\n");
-    printf("Une seule fois le même nombre par ligne/colonne.\n");
-    printf("Une seule fois le même nombre dans une sous-grille (tableau ayant un autre nombre de lignes/colonnes inférieur à celui de la grille).\n");
+    printf("Les regles sont les suivantes :\n");
+    printf("Une seule fois le meme nombre par ligne/colonne.\n");
+    printf("Une seule fois le meme nombre dans une sous-grille (tableau ayant un autre nombre de lignes/colonnes inferieur a celui de la grille).\n");
     printf("Notre jeu propose plusieurs options que tu pourra selectionner avant la partie.\n");
     printf("Normes de saisie du jeu :\n");
-    printf("Lors de la selectin des options, ecris o pour l'avoir ou n pour la desactiver.\n");
-    printf("Lors d'un coup, il faut écrire le numero de la ligne, puis le numero de la colonne et enfin le nombre que tu veux jouer.\n");
+    printf("Lors de la selectin des options, ecris 'o' pour l'avoir ou 'n' pour la desactiver.\n");
+    printf("Lors d'un coup, il faut ecrire le numero de la ligne, puis le numero de la colonne et enfin le nombre que tu veux jouer.\n");
     printf("Bon courage!\n");
 }
 
