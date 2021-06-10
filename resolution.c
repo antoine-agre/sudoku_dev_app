39b464e8161b88197830d084d22652443f7a62b4
William Denorme <80251305+Williamesta@users.noreply.github.com>
2021-06-10 11:06:28 +0200

    mineur

--- resolution.c
+++ resolution.c
@@ -7,7 +7,6 @@ Le cube contient des 1/0 pour 1 <= x <= MAX, 1 <= y <= MAX, 1 <= Z <= MAX; et de
 void printCube(Cube cube){
 //Affiche le contenu du cube, sous forme de plans successifs.
 //Utilisé uniquement en debug.
-
     for(int k = 0; k <= MAX; k++){
         printf("PLAN %d\n",k);
         for(int i = 0; i <= MAX+1; i++){
@@ -18,12 +17,10 @@ void printCube(Cube cube){
         }
         printf("\n\n");
     }
-
 }
 
 void initCube(Cube cube){
 //Initialise cube comme correspondant à une grille vide.
-
     //0 initiaux
     for(int i = 0; i <= MAX+1; i++){
         for(int j = 0; j <= MAX; j++){
@@ -32,7 +29,6 @@ void initCube(Cube cube){
             }
         }
     }
-
     //Grilles principales
     for(int i = 1; i <= MAX; i++){
         for(int j = 1; j <= MAX; j++){
@@ -41,14 +37,12 @@ void initCube(Cube cube){
             }
         }
     }
-
     //grille de compteurs du plan 0
     for(int i = 1; i <= MAX; i++){
         for(int j = 1; j <= MAX; j++){
             cube[i][j][0] = MAX;
         }
     }
-
     //compteurs des plans non-nuls
     for(int i = 0; i <= MAX+1; i+=MAX+1){
         for(int j = 1; j <= MAX; j++){
@@ -57,74 +51,57 @@ void initCube(Cube cube){
             }
         }
     }
-
     for(int i = 1; i <= MAX; i++){
         for(int k = 1; k <= MAX; k++){
             cube[i][0][k] = MAX;
         }
     }
 }
-
 void updateCube(Cube cube, int l, int c, int x){
 //Met à jour le cube pour un entier x placé aux coordonnées l c de la grille.
-
     //0 sur ligne
     /*for(int i = 1; i <= MAX; i++){
         cube[l][i][x] = 0;
     }
-
     //0 sur colonne
     for(int i = 1; i <= MAX; i++){
         cube[i][c][x] = 0;
     }
-
     //0 sur "colonne" plan
     for(int i = 1; i <= MAX; i++){
         cube[l][c][i] = 0;
     }*/
-
     for(int i = 1; i <= MAX; i++){
         cube[l][i][x] = 0;//0 sur ligne
         cube[i][c][x] = 0;//0 sur colonne
         cube[l][c][i] = 0;//0 sur "colonne" plan
     }
-
     //maj compteurs
     for(int i = 0; i <= MAX; i++){
         updateCompteurs(cube, i);
     }
-
 }
 
 void generateCube(Cube cube, Grid grid){
 //Génere un cube correspondant à la grille passée en paramètre.
-
     initCube(cube);
-
     for(int i = 0; i < MAX; i++){
         for(int j = 0; j < MAX; j++){
             //Pour chaque indice de la grille
             if(grid[i][j] != 0){
-
                 updateCube(cube, i+1, j+1, grid[i][j]);
                 //printCube(cube);
-
             }
         }
     }
-
 }
 
 void updateCompteurs(Cube cube, int plan){
 //Met à jour les compteurs du plan donné du cube.
-
     int count;
-
     if(plan == 0){
-
         for(int i = 1; i <= MAX; i++){
             for(int j = 1; j <= MAX; j++){
-
                 count = 0;
                 for(int k = 1; k <= MAX; k++){
                     if(cube[i][j][k] == 1){count++;}
@@ -133,47 +110,36 @@ void updateCompteurs(Cube cube, int plan){
             }
         }
     }
-
     else{
-
         //Lignes
         for(int i = 1; i <= MAX; i++){
-
             count = 0;
             for(int j = 1; j <= MAX; j++){
                 if(cube[i][j][plan] == 1){count++;}
             }
             cube[i][0][plan] = count;
         }
-
         //Colonnes
         for(int j = 1; j <= MAX; j++){
-
             count = 0;
             for(int i = 1; i <= MAX; i++){
                 if(cube[i][j][plan] == 1){count++;}
             }
             cube[0][j][plan] = count;
         }
-
         //Régions
         for(int i = 1; i <= sqrt(MAX); i++){
             for(int j = 1; j <= sqrt(MAX); j++){
                 count = 0;
-
                 for(int m = 1; m <= sqrt(MAX); m++){
                     for(int n = 1; n <= sqrt(MAX); n++){
                         if(cube[(int)(m+sqrt(MAX)*(i-1))][(int)(n+sqrt(MAX)*(j-1))][plan]==1){count++;}
-
                     }
                 }
                 cube[MAX+1][j+3*(i-1)][plan] = count;
-
             }
         }
-
     }
-
 }
 
 //void createCube pour une grille existante ?
