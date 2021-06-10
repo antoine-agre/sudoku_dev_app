e2c53333289439c8d5a48e787d74c7802fb0ac47
William Denorme <80251305+Williamesta@users.noreply.github.com>
2021-06-10 10:37:56 +0200

    intégration du header

--- Main.c
+++ Main.c
@@ -1,11 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#define MAX 4                  //dimension de la grille
-#define nbOption 4              //nombre d'options existantes dans notre programme  (possible de faire un tableau qui enregistre si les option sont selectionnées ou non ex : premiere case = 0 donc l'option n'est pas selectionnées)
-
-typedef int Grid[MAX][MAX];
-typedef int Cube[MAX+2][MAX+1][MAX+1];
+#include "Header.h"
 
 int main(){
     srand(time(NULL));           //mise en place de l'aléatoire
@@ -26,6 +19,5 @@ int main(){
     //saveGrid(grid);
     //Selection des options
     //boucle qui pour chaque itération fait jouer un coup au joueur et applique les options selectionnés précédement
-
     return 0;
 }
