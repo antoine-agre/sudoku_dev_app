f8909ec78d4a9578146ff33a0a7bf003224c0f84
William Denorme <80251305+Williamesta@users.noreply.github.com>
2021-06-20 15:29:20 +0200

    Update Interface GTK.c

--- Interface GTK.c
+++ Interface GTK.c
@@ -1,6 +1,5 @@
 #include "Header.h"
 
-typedef int Grid[MAX][MAX];
 FILE* fichier = NULL;
 
 static int grid[9][9];
@@ -10,15 +9,15 @@ bool to_show;
 
 
 bool isValidMove(Grid grid, int x, int y, int n){
-    //il faut des nombre avec une racine carré parfaite
-    //Vérifie si colonne + ligne + carré de la case x y ne contiennent jamais l'entier n (y compris case x y)
+    //il faut des nombre avec une racine carrÃ© parfaite
+    //VÃ©rifie si colonne + ligne + carrÃ© de la case x y ne contiennent jamais l'entier n (y compris case x y)
     for(int i = 0; i < MAX; i++){ //ligne
         if(grid[x][i] == n){return false;}
     }
     for(int i = 0; i < MAX; i++){//colonne
         if(grid[i][y] == n){return false;}
     }
-    // k et l : décalage ligne/colonne
+    // k et l : dÃ©calage ligne/colonne
     int k,l;
     if(x >= 0 && x < sqrt(MAX)){k = 0;}
     else if(x >= sqrt(MAX) && x < 2*sqrt(MAX)){k = sqrt(MAX);}
